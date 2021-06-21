b2bb9cc5c0ea6388c04064b25491ce77ca820a63
Anthony Rappa <rappa819@gmail.com>
2021-06-21 17:01:50 -0400

    Update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Livewire Tables Demo</h1>
+        <h1 class="display-5 fw-bold mb-5"><a href="https://github.com/rappasoft/laravel-livewire-tables" target="_blank">Livewire Tables</a> Demo</h1>
 
         <div class="col-lg-6 mx-auto">
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
