64fdf8d17a7066599764fa91e9c42dfb07664994
lrljoe <joe@lowerrocklabs.com>
2023-03-31 01:47:27 +0100

    Initial Commit - Adding SlideDown Option

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+import '@nextapps-be/livewire-sortablejs';
+window.Alpine = Alpine;
+window.Alpine.plugin(focus);
+window.Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,34 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+Route::get('/tailwind', function () {
+    return view('tw');
+})->name('tw');
+
+Route::get('/tailwind-slidedown', function () {
+    return view('tw-slidedown');
+})->name('tw-slidedown');
+
+Route::get('/tailwind3', function () {
+    return view('tw3');
+})->name('tw3');
+
+Route::get('/tailwind3-slidedown', function () {
+    return view('tw3-slidedown');
+})->name('tw3-slidedown');
+
 Route::get('/bootstrap-4', function () {
     return view('bs4');
 })->name('bs4');
+
+Route::get('/bootstrap-4-slidedown', function () {
+    return view('bs4-slidedown');
+})->name('bs4-slidedown');
 
 Route::get('/bootstrap-5', function () {
     return view('bs5');
 })->name('bs5');
 
-Route::get('/tailwind', function () {
-    return view('tw');
-})->name('tw');
+Route::get('/bootstrap-5-slidedown', function () {
+    return view('bs5-slidedown');
+})->name('bs5-slidedown');
